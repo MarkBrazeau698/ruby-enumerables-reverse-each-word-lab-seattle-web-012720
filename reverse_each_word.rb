1bd9f65b5a5d9e0874f9a85be8d69bdf4cb2bdc2
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-10 05:53:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,8 @@ require "pry"
 def reverse_each_word(string)
   new = []
   string_array = string.split
-  
   string_array.each do |word|
-  #binding.pry
   new << word.reverse
-  
   end
  new.join(" ")
  end
