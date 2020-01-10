30297a3f04c1cd0ff406004c4a1790a7720cdcc3
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-10 05:44:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,10 +8,10 @@ require "pry"
 
 def reverse_each_word(string)
   new = []
-  new << string
+  string_array = string.split
   binding.pry
-  new.reverse_each do |n|
-  #"#{n.each_with_object}"
+  string_array.each do |word|
+  
   end
  new
  end
